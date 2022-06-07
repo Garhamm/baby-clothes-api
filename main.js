8126fd54bda8beec3aae0fea2fe84051214797ef
Garrett Hamman <50977644+Garhamm@users.noreply.github.com>
2022-06-07 15:17:44 -0500

    more changes to return weather info

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const rapperName = document.querySelector('input').value
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=38.98&lon=-94.67&appid=4002060b1a81792c61fbc96fe60db2d0`)
         const data = await response.json()
-
-        console.log(data)
-        document.querySelector('h2').innerText = data.birthName
     }catch(error){
         console.log(error)
     }
-}
+}   
